baf563dcaa15074bfd25dc1545799d8aa304c525
AndrewLBeasley <alloydbeasley@gmail.com>
2019-06-10 18:52:33 -0500

    inbox tests

--- inbox/compile.js
+++ inbox/compile.js
@@ -4,7 +4,7 @@ const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
-const contract = solc.compile(source, 1).contracts['InboxContract'];
+const contract = solc.compile(source, 1).contracts[':InboxContract'];
  
 
 module.exports = contract;
