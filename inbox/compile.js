ac0f4f69cc0bd9a210d85e726e84ea9e9584527e
AndrewLBeasley <alloydbeasley@gmail.com>
2019-10-21 21:30:36 -0500

    fixed versioning issues, tests passing

--- inbox/compile.js
+++ inbox/compile.js
@@ -2,9 +2,10 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 const contract = solc.compile(source, 1).contracts[':InboxContract'];
  
-
 module.exports = contract;
